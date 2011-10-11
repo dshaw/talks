25045f70a77e6020249508773d52b460788e816f
Daniel Shaw <github@dshaw.com>
2011-10-11 12:13:41 -0700

    Tighten up app and introduce `socket.io-announce`.

--- 2011-10-jsclub/sample-app/exec.js
+++ 2011-10-jsclub/sample-app/exec.js
@@ -1,11 +1,20 @@
 var spawn = require('child_process').spawn;
 
-var count = process.argv[2] || 4 // maxes out locally at 82
+var count = process.argv[2] || 4 // maxes out locally at ~82
   , nodes = {
-      io: []
+        announce: []
+      , io: []
     }
   , ioPort = 8881;
 
+// announce data server
+nodes.announce.push(spawn('node', ['data.js']));
+console.log(
+    'announce'
+  , 'pid:', nodes.announce[0].pid
+);
+
+// socket.io instances
 for (var i=0; i<count; i++) {
   var port = ioPort+i
     , nodeId = i+1;
